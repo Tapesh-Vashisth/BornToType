759bd14baa491f5d5d7a1c492c6fe0d99dada476
Pranav Gupta <pranav.gupta@raybusiness.onmicrosoft.com>
2022-11-10 16:22:19 +0530

    changes to terms

--- btt/src/pages/Terms.tsx
+++ btt/src/pages/Terms.tsx
@@ -21,17 +21,6 @@ const Terms = () => {
             <br />
             <p>Typo reserves the right to remove or disable any account or any other content on this site at any time for any reason, without prior notice to you, if we believe that you have violated this agreement.</p>
             <br />
-            <p>You agree that you will not upload, post, host, or transmit any content that:</p>
-            <ol>
-                <li>is unlawful or promotes unlawful activities;</li>
-                <li>is or contains sexually obscene content;</li>
-                <li>is libelous, defamatory, or fraudulent;</li>
-                <li>is discriminatory or abusive toward any individual or group;</li>
-                <li>is degrading to others on the basis of gender, race, class, ethnicity, national origin, religion, sexual preference, orientation, or identity, disability, or other classification, or otherwise represents or condones content that: is hate speech, discriminating, threatening, or pornographic; incites violence; or contains nudity or graphic or gratuitous violence;</li>
-                <li>violates any person's right to privacy or publicity, or otherwise solicits, collects, or publishes data, including personal information and login information, about other Users without consent or for unlawful purposes in violation of any applicable international, federal, state, or local law, statute, ordinance, or regulation; or</li>
-                <li>contains or installs any active malware or exploits/uses our platform for exploit delivery (such as part of a command or control system); or infringes on any proprietary right of any party, including patent, trademark, trade secret, copyright, right of publicity, or other rights.</li>
-            </ol>
-            <br />
             <p>
             While using the Services, you agree that you will not:
             </p>
@@ -45,12 +34,8 @@ const Terms = () => {
                 <li>access the Services in ways that exceed your authorization;</li>
                 <li>falsely impersonate any person or entity, including any of our contributors, misrepresent your identity or the site's purpose, or falsely associate yourself with Typo;
                 </li>
-                <li>violates any person's right to privacy or publicity, or otherwise solicits, collects, or publishes data, including personal information and login information, about other Users without consent or for unlawful purposes in violation of any applicable international, federal, state, or local law, statute, ordinance, or regulation; or</li>
-                <li>violate the privacy of any third party, such as by posting another person's personal information without their consent;
-                </li>
+                <li>violates any person's right to privacy or publicity, or otherwise solicits, collects, or publishes data, including personal information and login information, about other Users without consent or for unlawful purposes in violation of any applicable international, federal, state, or local law, statute, ordinance.</li>
             </ol>
-            <br />
-            <p>Without limiting the foregoing, you will not transmit or post any content anywhere on the Services that violates any laws. Typo absolutely does not tolerate engaging in activity that significantly harms our Users. We will resolve disputes in favor of protecting our Users as a whole.</p>
             <br />
             <h2>Links</h2>
             <p>Typo is not responsible for the contents of any linked sites. The use of any linked website is at the user's own risk.
