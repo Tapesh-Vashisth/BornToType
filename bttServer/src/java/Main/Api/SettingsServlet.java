9ccd3e146e3aa594ebd9930c03dd53ef88686cb0
tapesh <tapeshvashisth789@gmail.com>
2022-11-22 13:32:49 +0530

    react

--- bttServer/src/java/Main/Api/SettingsServlet.java
+++ bttServer/src/java/Main/Api/SettingsServlet.java
@@ -20,7 +20,36 @@ public class SettingsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            System.out.println("hello");
+            BufferedReader br = 
+                 new BufferedReader(new InputStreamReader(request.getInputStream()));
+
+            String json = "";
+            if(br != null){
+                json = br.readLine();
+            }
+            Object file = JSONValue.parse(json);
+            
+            System.out.println(json);
+            
+            JSONObject jsonObjectCode = (JSONObject)file;
+            Connect con;
+            
+            try {
+                con = new Connect();
+                con.connect();
+                Connection conn = con.getConnector();
+                String username = jsonObjectCode.get("username").toString();
+                int fontSize = Integer.parseInt(jsonObjectCode.get("fontSize").toString());
+                int theme = Integer.parseInt(jsonObjectCode.get("theme").toString());
+                String fontfamily = jsonObjectCode.get("fontFamily").toString();
+                System.out.println("hello");
+                Statement stmt = conn.createStatement();
+                stmt.executeUpdate("update user_settings set fontSize = " + fontSize + ", theme = " + theme + ", fontfamily = '" + fontfamily + "'where username = '" + username + "'");
+                response.setStatus(200);
+            }catch (SQLException e){
+                System.out.println(e);
+                response.sendError(403);
+            }
     }
     
     @Override
